610fa9700b1a84cef1e6d461ddc7daaae1f7b028
jl123567 <ledbetterte@gmail.com>
2018-02-14 12:13:00 -0500

    cented popup
    used this:
    <https://stackoverflow.com/questions/14910858/how-to-specify-where-a-tkinter-window-opens/14912644>

--- popup test.py
+++ popup test.py
@@ -6,11 +6,17 @@ NORM_FONT = ("Verdana", 12)
 
 def popupmsg(msg):
     popup = tk.Tk()
+    sw = popup.winfo_screenwidth()
+    sh = popup.winfo_screenheight()
     popup.wm_title("!")
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    # centering help from <https://stackoverflow.com/questions/14910858/how-to-specify-where-a-tkinter-window-opens
+    # /14912644>
+    popup.geometry('%dx%d+%d+%d' % (100, 80, ((sw / 2) - 50), ((sh / 2) - 40)))
+    label = ttk.Label(popup, text=str(msg), font=NORM_FONT)
     label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Okay", command=popup.destroy)
-    B1.pack()
+    b1 = ttk.Button(popup, text="Okay", command=popup.destroy)
+    b1.pack()
+
     popup.mainloop()
 
 
