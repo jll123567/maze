fe6462a963c3f36520dba3a7b70c94cfed4d9840
jl123567 <ledbetterte@gmail.com>
2018-02-14 10:29:57 -0500

    turtle test update
    turtle test is my test of drawing and moving turtles
    
    maze test is a test at drawing and moving arround a small maze

--- tutle test.py
+++ tutle test.py
@@ -1,14 +1,17 @@
 # import
 import turtle
-from time import sleep
 
 # init screen, turtle
 window = turtle.Screen()
 turt = turtle.Turtle()
+moveable = turtle.Turtle(shape="turtle")
 turt.speed(5)
+moveable.speed(2)
+moveable.color("green", "green")
+
 
 # turtle instructions
-for _ in range(0, 10):
+for _ in range(0, 3):
     for _ in range(0, 18):
         turt.forward(50)
         turt.left(20)
@@ -16,10 +19,27 @@ for _ in range(0, 10):
     for _ in range(0, 18):
         turt.forward(50)
         turt.right(20)
-
 turt.ht()
-sleep(3)
-turtle.bye()
+
+
+# key functions
+def up():
+    moveable.forward(10)
+
+
+def left():
+    moveable.left(10)
+
+
+def right():
+    moveable.right(10)
+
+
+# key events
+window.onkeypress(up, "w")
+window.onkeypress(left, "a")
+window.onkeypress(right, "d")
 
 # window await
+window.listen()
 window.mainloop()
